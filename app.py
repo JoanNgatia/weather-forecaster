c715b24778ead534b166d33b480af62c3d00aa22
andela-jngatia <joan.ngatia@andela.com>
2016-08-10 12:38:53 +0300

    [Feature]Current Day Weather forecast complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-import json
+# import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -15,23 +15,20 @@ def main():
 @app.route("/city", methods=['GET', 'POST'])
 def get_city():
     city_name = request.args['city']
-    payload = {'APPID': API_KEY, 'q': city_name}
+    payload = {'APPID': API_KEY, 'q': city_name, 'units': 'metric'}
     if city_name:
         r = requests.get(
             'http://api.openweathermap.org/data/2.5/weather', params=payload)
-        # print r.url
         y = r.json()
+        wind = y['wind']['speed']
         weather = y['weather'][0]['main']
-        return render_template("city.html", city=city_name, weather=weather)
+        average_temp = y['main']['temp']
+        return render_template("index.html", city=city_name,
+                               weather=weather, windspeed=wind,
+                               temp=average_temp)
     else:
         return render_template("failed.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# >>> r = requests.get('http://httpbin.org/get', params=payload)
-# print(r.url)
-# http://httpbin.org/get?key2=value2&key1=value1
